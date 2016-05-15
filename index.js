8d6f0ab208c454f8b73002301700ae1e39112cbf
brideout <bret@rapidwarellc.com>
2016-05-15 00:34:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = 'mongodb://heroku_4xdgw3jw:l6g5qikj97i8eor09i3k9qja6v@ds013908.mongolab.com:13908/heroku_4xdgw3jw';
 
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
 
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'thewrapappdlIeo58sl149jNeroU34',
+  masterKey: process.env.MASTER_KEY || 'kgfiW2955dPfkLKwnbe82', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
